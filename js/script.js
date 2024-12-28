840d41040b816a8b4ddb34891f2bafa98c18590f
eliaszavan <eliaszavan@gmail.com>
2024-12-28 10:31:59 -0300

    Added CNY and EUR to Currency Converter + Style updated

--- js/script.js
+++ js/script.js
@@ -1,8 +1,43 @@
 let amount = document.querySelector(".amount");
 let button = document.querySelector("button");
 let result = document.querySelector(".result");
+let p = document.querySelector(".title-result");
+
+let selector = document.querySelector(".currency-selector");
+
+async function setText(text) {
+    result.textContent = text;
+}
+
+let currencySelected = selector.options[selector.selectedIndex].value;
+let currencySymbol = getCurrencySymbol(currencySelected);
+
+//Configurando textos
+setText(`${currencySymbol}0.00`);
+p.textContent = `Resultado em ${currencySelected}:`;
+
+selector.addEventListener("change", (s) => {
+    //Atualizando variáveis
+    currencySelected = selector.options[selector.selectedIndex].value;
+    currencySymbol = getCurrencySymbol(currencySelected);
+    setText(`${currencySymbol}0.00`);
+    p.textContent = `Resultado em ${currencySelected}:`;
+});
+
+//Funções
+function getCurrencySymbol(currencyCode) {
+    const symbols = {
+        USD: "$",
+        CNY: "¥",
+        EUR: "€",
+        BRL: "R$"
+    };
+    return symbols[currencyCode] || "";
+}
 
 async function convert() {
+    currencySelected = selector.options[selector.selectedIndex].value
+
     try {
         // URL da API para obter a taxa de câmbio de BRL para USD
         const response = await fetch("https://api.exchangerate-api.com/v4/latest/BRL");
@@ -12,25 +47,29 @@ async function convert() {
 
         const data = await response.json();
 
-        // Obtendo a taxa de conversão BRL para USD
-        const rate = data.rates.USD;
+        //Obtendo a conversão
+        const rate = data.rates[currencySelected];
+        if (!rate) {
+            setText("Moeda selecionada inválida.");
+            return;
+        }
 
         // Convertendo o valor inserido
         const brlAmount = parseFloat(amount.value);
         if (isNaN(brlAmount)) {
-            result.textContent = "Por favor, insira um valor válido.";
+            setText("Por favor, insira um valor válido.");
             return;
         }
 
         const convertedAmount = brlAmount * rate;
+        const currencySymbol = getCurrencySymbol(currencySelected);
 
         // Exibindo o resultado formatado
-        result.textContent = `$${convertedAmount.toFixed(2)}`;
+        result.textContent = `${currencySymbol}${convertedAmount.toFixed(2)}`;
     } catch (error) {
         console.error(error);
-        result.textContent = "Erro ao realizar a conversão. Tente novamente mais tarde.";
+        setText("Erro ao realizar a conversão. Tente novamente mais tarde.");
     }
-    result.textContent = `$` + data.result.toFixed(2);
 }
 
 button.addEventListener("click", (e) => {
